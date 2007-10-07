05d10e954d1c3f531e187122666c3e4981200619
sonnyg95 <sonnyg95@1983c6a9-b83b-0410-aa89-8901dee73480>
2007-10-07 12:36:18 +0000

    Previous Version passed verify but was not 100% covered with Emma.  This new version passes verify and is also has 100% coverage according to Emma

--- src/edu/hawaii/myisern/example/TestMyIsernXmlLoader.java
+++ src/edu/hawaii/myisern/example/TestMyIsernXmlLoader.java
@@ -2,6 +2,7 @@ package edu.hawaii.myisern.example;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
@@ -9,7 +10,7 @@ import org.junit.Test;
  * Demonstrates that the sample code is working. 
  * @author Philip Johnson
  */
-public class TestMyIsernXmlLoader {
+public class TestMyIsernXmlLoader extends TestCase {
 
   /**
    * Tests the Collaboration XML to Java conversion. 
@@ -21,6 +22,18 @@ public class TestMyIsernXmlLoader {
     assertEquals("Check organizations size", 2, loader.getNumOrganizations());
     assertEquals("Check researchers size", 3, loader.getNumResearchers());
     assertTrue("Check Collaborations", loader.getCollaborationsXml().contains("<Collaborations>"));
+  }
+  
+  /**
+   * Tests main method.
+   * @throws Exception if problem occurs.
+   */
+  @Test
+  public void testMain() throws Exception {
+    String[] args = new String[3];
+    args[0] = "-researchers";
+    MyIsernXmlLoader.main(args);
+    assertNotNull("Check main", args);
   }
 
 }
