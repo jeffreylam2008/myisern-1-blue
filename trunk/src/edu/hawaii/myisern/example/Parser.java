0556e5ab7bae076867998b950baf2f228707da0a
sonnyg95 <sonnyg95@1983c6a9-b83b-0410-aa89-8901dee73480>
2007-10-14 11:04:58 +0000

    Added checks for first and second Argument.  Verify passed but Emma is not 100% covered.  Below is the Emma Coverage Summary.    [concat] Emma Coverage summary    [concat] class:   100% (3/3)    [concat] method:  67%  (10/15)    [concat] block:   41%  (349/860)    [concat] line:    41%  (85/206)
    This is due to the lack of testing for the new methods I've added.  I'm planning on fixing this tomorrow morning.

--- trunk/src/edu/hawaii/myisern/example/Parser.java
+++ trunk/src/edu/hawaii/myisern/example/Parser.java
@@ -11,6 +11,9 @@ public class Parser {
   boolean isCollaborationsOn = false;
   boolean isOrganizationsOn = false;
   boolean isResearchersOn = false;
+  boolean firstArgumentPass = false;
+  boolean secondArgumentPass = false;
+  boolean thirdArgumentPass = false;
   
   int booleanCounter;
   int argsCounter;
@@ -23,8 +26,21 @@ public class Parser {
   Parser(String[] args) {
     this.booleanCounter = 0;
     this.argsCounter = 0;
-
-    //Checks for user input
+    
+/*
+    
+    //Check if first argument is valid input
+    this.firstArgumentPass = checkFirstArgument(args);
+    
+    
+    if (firstArgumentPass) {
+      System.out.println("First Argument Pass");
+    }
+    else {
+      this.printHelp();
+    } */
+    
+  /*  
     for (String commandLine : args) {
       if ("--help".equals(commandLine)) {
         argsCounter = 0;
@@ -47,7 +63,70 @@ public class Parser {
         booleanCounter++;
       }
       argsCounter++;
+    } */
+  }
+  
+  /**
+   * Checks second argument to see if it is valid.
+   * @param args containing command line arguments.
+   * @return secondArgumentPass if second argument entered is valid.
+   */
+  public boolean checkSecondArgument (String[] args) {
+    boolean secondArgumentPass = false;
+    
+    try {
+      if ("-organization".equals(args[1])) {
+        secondArgumentPass = true;
+      }
+      else if ("-year".equals(args[1])) {
+        secondArgumentPass = true;
+      }
+      else if ("-researcher".equals(args[1])) {
+        secondArgumentPass = true;
+      }
+      else if ("-collaboration".equals(args[1])) {
+        secondArgumentPass = true;
+      }
+      else if ("-all".equals(args[1])) {
+        secondArgumentPass = true;
+      }
+      else if ("-collaborationLevelEquals".equals(args[1])
+          || ("-collaborationLevelGreaterThan".equals(args[1]))) {
+        secondArgumentPass = true;
+      }
     }
+    catch (Exception e) {
+      secondArgumentPass = false;
+    }
+    
+    return secondArgumentPass;
+  }
+  
+  /**
+   * Checks first argument if it is valid.
+   * @param args containing command Line arguments.
+   * @return firstArgumentPass if first argument given is valid.
+   */
+  public boolean checkFirstArgument (String[] args) {
+    boolean firstArgumentPass = false;
+    try {
+      if ("-describe".equals(args[0])) {
+        //Display
+        firstArgumentPass = true;
+      } 
+      else if ("-listCollaborations".equals(args[0])) {
+        //list only collaborations
+        firstArgumentPass = true;
+      }
+      else if ("-listOrganizations".equals((args[0]))) {
+        //list only organizations
+        firstArgumentPass = true;
+      }
+    }
+    catch (ArrayIndexOutOfBoundsException e) {
+      firstArgumentPass = false;
+    }
+    return firstArgumentPass;
   }
   
   /**
@@ -58,9 +137,27 @@ public class Parser {
     System.out.println("Provides sample code for " + 
       "loading XML and marshalling it into their JAXB related classes.");
     System.out.println("\nUsage: MyIsernXmlLoader [OPTION]");
-    System.out.println("\t-c, --printCollaborations\tprints collaborations.");
+    
+    System.out.println("\t-listCollaborations -organization <uniqueID>" +
+    		"\n\t\tLists Collaborations known for specified organization\n");
+    System.out.println("\t-listCollaborations -year <year>" +
+    		"\n\t\tLists Collaborations known for specified year\n");
+    System.out.println("\t-listCollaborations -researcher <uniqueID>" +
+    		"\n\t\tLists Collaborations known for specified researcher\n");
+    
+    System.out.println("\t-describe -researcher <uniqueID>");
+    System.out.println("\t-describe -organization <uniqueID>");
+    System.out.println("\t-describe -collaboration <uniqueID>");
+    System.out.println("\t-describe -all Researchers");
+    System.out.println("\t-describe -all Organizations");
+    System.out.println("\t-describe -all Collaborations");
+    
+    System.out.println("\t-listOrganizations -collaborationLevelEquals <integer>");
+    System.out.println("\t-listOrganizations -collaborationLevelGreaterThan <integer>");
+   
+    /*System.out.println("\t-c, --printCollaborations\tprints collaborations.");
     System.out.println("\t-o, --printOrganizations\tprints organizations.");
     System.out.println("\t-r, --printResearchers\t\tprints researchers.");
-    System.out.println("\t    --help\t\t\tdisplay this help and exits.");
+    System.out.println("\t    --help\t\t\tdisplay this help and exits.");*/
   }
 }
