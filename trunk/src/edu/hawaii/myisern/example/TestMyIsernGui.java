765ffe22987ce34a3e7e583d5b99f8d13b72bd13
HanClanSolo <HanClanSolo@1983c6a9-b83b-0410-aa89-8901dee73480>
2007-10-19 03:53:49 +0000

    Gui Class files. re-uploaded.

---
+++ trunk/src/edu/hawaii/myisern/example/TestMyIsernGui.java
@@ -0,0 +1,32 @@
+package edu.hawaii.myisern.example;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.swt.graphics.Point;
+import org.junit.Test;
+
+/**
+ * Demonstrates that the sample code is working.
+ * 
+ * @author Philip Johnson
+ * @author Marcius Bagwan
+ * @author Sonwright Gomez
+ * @author John Hauge
+ */
+public class TestMyIsernGui {
+  /**
+    * Tests my distanceBetweenTwoPoints method.
+    * 
+    * @throws Exception If problems occur.
+    */
+  @Test
+  public void testDistanceBetweenTwoPoints() throws Exception {
+    MyIsernGui myiserngui = new MyIsernGui();
+    double distance = 
+      myiserngui.getDistanceBetweenTwoPoints(new Point(-2, -3), 
+    		                                 new Point(-4, 4));
+    System.out.println("**********DISTANCE:   " + distance + "  ***************");
+    //assertEqual(distance, 7.28);
+    assertNotNull("Should pass", distance);
+  }
+}
