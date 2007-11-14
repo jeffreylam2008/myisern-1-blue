4836d9d3df8b6c2ed0b08cd20373807c27335408
denverjockstrap <denverjockstrap@1983c6a9-b83b-0410-aa89-8901dee73480>
2007-11-14 06:53:22 +0000

    [Issue 6][Issue 7] Modified implementation of print methods and UI appearance.

--- trunk/src/edu/hawaii/myisern/example/MyIsern.java
+++ trunk/src/edu/hawaii/myisern/example/MyIsern.java
@@ -32,7 +32,7 @@ import edu.hawaii.myisern.researchers.jaxb.Researchers;
  */
 public class MyIsern {
   /** MyIsern's own xml loader */
-  private MyIsernXmlLoader mixl;
+  public MyIsernXmlLoader mixl;
   /** MyIsern's own xml saver */
   MyIsernXmlSaver mixs;
   /** Researcher object that will hold all new and old researchers */
@@ -61,16 +61,16 @@ public class MyIsern {
    * 
    * @return An iterator over the list of all researchers.
    */
-  public Iterator<Object> printResearchers() {
+  public List<String> printResearchers() {
     List<Researcher> researcherList;
     researcherList = this.rList.getResearcher();
-    List<Object> elementsList = new ArrayList<Object>();
+    List<String> elementsList = new ArrayList<String>();
 
     for (Researcher currentResearcher : researcherList) {
       elementsList.add(currentResearcher.getName());
     }
     
-    return elementsList.iterator();
+    return elementsList;
   }
 
   /**
@@ -79,16 +79,16 @@ public class MyIsern {
    * 
    * @return An iterator over the list of all organizations.
    */
-  public Iterator<Object> printOrganizations() {
+  public List<String> printOrganizations() {
     List<Organization> organizationList;
     organizationList = this.oList.getOrganization();
-    List<Object> elementsList = new ArrayList<Object>();
+    List<String> elementsList = new ArrayList<String>();
 
     for (Organization current : organizationList) {
       elementsList.add(current.getName());
     }
     
-    return elementsList.iterator();
+    return elementsList;
   }
 
   /**
@@ -97,16 +97,16 @@ public class MyIsern {
    * 
    * @return An iterator over the list of all collaborations.
    */
-  public Iterator<Object> printCollaborations() {
+  public List<String> printCollaborations() {
     List<Collaboration> collaborationList;
     collaborationList = cList.getCollaboration();
-    List<Object> elementsList = new ArrayList<Object>();
+    List<String> elementsList = new ArrayList<String>();
 
     for (Collaboration current : collaborationList) {
       elementsList.add(current.getName());
     }
     
-    return elementsList.iterator();
+    return elementsList;
   }
   
   /**
@@ -115,96 +115,24 @@ public class MyIsern {
    * @param id The id of the researcher to be printed.
    * @return A list of a list of objects containing the specified researcher's data.
    */
-  public List<List<Object>> printResearcher(String id) {
-    List<Object> researcherName = new ArrayList<Object>();
-    List<Object> researcherOrganization = new ArrayList<Object>();
-    List<Object> researcherBioStatement = new ArrayList<Object>();
-    List<Object> researcherPictureLink = new ArrayList<Object>();
-    List<Object> researcherEmail = new ArrayList<Object>();
-    List<List<Object>> researcherDataList = new ArrayList<List<Object>>();
-    
+  public Iterator<Object> printResearcher(String id) {
+    List<Object> researcherData = new ArrayList<Object>();
     for (Researcher currentResearcher : this.mixl.getResearchers().getResearcher()) {
       if (currentResearcher.getName().replace(' ', '_').equals(id)) {
-        researcherName.add(currentResearcher.getName());
-        researcherDataList.add(researcherName);
         
-        researcherOrganization.add(currentResearcher.getOrganization());
-        researcherDataList.add(researcherOrganization);
+        researcherData.add(currentResearcher.getName());
         
-        researcherBioStatement.add(currentResearcher.getBioStatement());
-        researcherDataList.add(researcherBioStatement);
+        researcherData.add(currentResearcher.getOrganization());
         
-        researcherPictureLink.add(currentResearcher.getPictureLink());
-        researcherDataList.add(researcherPictureLink);
+        researcherData.add(currentResearcher.getBioStatement());
         
-        researcherEmail.add(currentResearcher.getEmail());
-        researcherDataList.add(researcherEmail);
+        researcherData.add(currentResearcher.getPictureLink());
+        
+        researcherData.add(currentResearcher.getEmail());
+        
+        break;
       }
-      break;
     }
-    return researcherDataList;
-  }
-  
-  /**
-   * Prints the Organization table for the specified Id if it is found in the Id list.
-   * 
-   * @param id The id of an organization to be printed.
-   * @return A list of a list of objects containing the specified organization's data.
-   */
-  public List<List<Object>> printOrganization(String id) {
-    List<Object> organizationName = new ArrayList<Object>();
-    List<Object> organizationType = new ArrayList<Object>();
-    List<Object> organizationContact = new ArrayList<Object>();
-    List<Object> organizationAffiliatedResearchers = new ArrayList<Object>();
-    List<Object> organizationCountry = new ArrayList<Object>();
-    List<Object> organizationKeywords = new ArrayList<Object>();
-    List<Object> organizationDescription = new ArrayList<Object>();
-    List<Object> organizationHomepage = new ArrayList<Object>();
-    List<List<Object>> organizationDataList = new ArrayList<List<Object>>();
-    
-    for (Organization current : this.mixl.getOrganizations().getOrganization()) {
-      if (current.getName().replace(' ', '_').equals(id)) {
-        List<String> stringList;
-        organizationName.add(current.getName());
-        organizationDataList.add(organizationName);
-
-        organizationType.add(current.getType());
-        organizationDataList.add(organizationType);
-
-        organizationContact.add(current.getContact());
-        organizationDataList.add(organizationContact);
-
-        // Prints all affiliated researchers with organization
-        AffiliatedResearchers affiliatedResearchers;
-        affiliatedResearchers = current.getAffiliatedResearchers();
-        stringList = affiliatedResearchers.getAffiliatedResearcher();
-        
-        for (String currentString : stringList) {
-          organizationAffiliatedResearchers.add(currentString);
-        }
-        organizationDataList.add(organizationAffiliatedResearchers);
-
-        organizationCountry.add(current.getCountry());
-        organizationDataList.add(organizationCountry);
-
-        // Prints Research Keywords for organization
-        ResearchKeywords researchKeywords;
-        researchKeywords = current.getResearchKeywords();
-        stringList = researchKeywords.getResearchKeyword();
-
-        for (String currentString : stringList) {
-          organizationKeywords.add(currentString);
-        }
-        organizationDataList.add(organizationKeywords);
-
-        organizationDescription.add(current.getResearchDescription());
-        organizationDataList.add(organizationDescription);
-
-        organizationHomepage.add(current.getHomePage());
-        organizationDataList.add(organizationHomepage);
-      }
-      break;
-    }
-    return organizationDataList;
+    return researcherData.iterator();
   }
 }
--- trunk/web/view_collaboration.jsp
+++ trunk/web/view_collaboration.jsp
@@ -1,5 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="stripes" uri="http://stripes.sourceforge.net/stripes.tld"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
@@ -29,21 +30,25 @@
    <td>
     <table align="center">
       <tr>
-        <td bgcolor="#6699FF" width="40"><h4><b>Collaboration</b></h4></td>
-        <td bgcolor="#666699" width="50"><stripes:link href="/view_organization.jsp"><h4><font color="#FFFFFF">Organization</font></h4></stripes:link></td>
-        <td bgcolor="#666699" width="50"><stripes:link href="/view_researcher.jsp"><h4><font color="#FFFFFF">Researcher</font></h4></stripes:link></td>
+        <stripes:form action="/MyIsern.action">
+           <td bgcolor="#666699" width="40"><stripes:submit name="collabLink" value="Collaboration"/></td>
+        </stripes:form> 
+        <stripes:form action="/MyIsern.action">
+           <td bgcolor="#666699" width="40"><stripes:submit name="orgLink" value="Organization"/></td>
+        </stripes:form> 
+        <stripes:form action="/MyIsern.action">
+           <td bgcolor="#666699" width="40"><stripes:submit name="resLink" value="Researcher"/></td>
+        </stripes:form>
       </tr>
     </table>
     <table align="center" border="1">
       <tr>
-        <td bgcolor="#99CCFF">Name</td>
-        <td>Collaborating-Organizations</td>
-        <td bgcolor="#99CCFF">Collaboration-Type</td>
-        <td>Years</td>
-        <td bgcolor="#99CCFF">Outcome-Types</td>
-        <td>Description</td>
-      </tr>
-    </table>
+        <td bgcolor="#99CCFF">Collaboration Name</td>  
+      </tr> 
+      <c:forEach var="element" items="${actionBean.collaborations}">
+     <tr><td>${element}</td></tr>
+   </c:forEach>
+      </table>
    </td>
   </tr>
 </table>
